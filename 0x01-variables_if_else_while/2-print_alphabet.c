f56f80b14de7055e8d9133967e0da2ce084b622b
root <lonickomanga>
2022-03-12 06:34:19 -0800

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+  char x[26] = "abcdefghijklmnopqrstuvwxyz";
+  int i;
+
+  for (i = 0; i < 26; i++)
+    {
+      putchar(x[i]);
+    }
+  putchar('\n');
+  return (0);
+}
