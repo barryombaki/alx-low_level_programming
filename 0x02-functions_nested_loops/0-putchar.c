cf86a5eb158e60f6786d0bbb44aa06c68f57411d
barryombaki <lonickomanga@gmail.com>
2022-03-15 03:24:07 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+// C program to demonstrate putchar() method
+
+#include <stdio.h>
+
+int main()
+{
+
+  // Get the character to be written
+  char ch = 'G';
+
+  // Write the Character to stdout
+  putchar(ch);
+
+  return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nested in c programming 
