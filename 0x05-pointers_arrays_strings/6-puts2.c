b709cb0507bf5844ab2b143726f5f17cb08cc4c8
barryombaki <lonickomanga@gmail.com>
2022-03-22 05:06:35 -0700

    pointers in c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of an input
+ * @str: string passed to the function
+ *
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(*str);
+
+		++i;
+		str++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+
+C - Pointers,
+
+arrays
+
+strngs
