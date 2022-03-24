778f7c42982967bb10cb9f827b5e21746599f7e5
barryombaki <lonickomanga@gmail.com>
2022-03-24 08:12:40 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize strings
+ *
+ * @ch: String co capitalize
+ *
+ * Return: Capitalized string
+ */
+
+char *cap_string(char *ch)
+{
+	int i = 0;
+
+	while (ch[i])
+	{
+		if (i == 0 && (ch[i] >= 97 && ch[i] <= 122))
+		{
+			ch[i] = ch[i] - 32;
+		}
+		else if (ch[i - 1] == 32 || ch[i - 1] == 9 || ch[i - 1] == 11 ||
+				ch[i - 1] == '\n' || ch[i - 1] == '.' || ch[i - 1] == ',' ||
+				ch[i - 1] == ';' || ch[i - 1] == '!' || ch[i - 1] == '?' ||
+				ch[i - 1] == '"' || ch[i - 1] == '(' || ch[i - 1] == ')' ||
+				ch[i - 1] == '{' || ch[i - 1] == '}')
+		{
+			if (ch[i] >= 97 && ch[i] <= 122)
+			{
+				ch[i] = ch[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (ch);
+}
