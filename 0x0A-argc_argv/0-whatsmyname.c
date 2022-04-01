f4530cadcfd1258d21af5cb2573d3f14bc184a2d
barryombaki <lonickomanga@gmail.com>
2022-04-01 04:23:36 -0400

    args argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * C Program to Print the Program Name and All its Arguments
+ */
+#include <stdio.h>
+ 
+void main(int argc, char *argv[])    /* command line Arguments */
+{
+    int i;
+    for (i = 0;i < argc;i++)
+    {
+        printf("%s ", argv[i]);        /* Printing the string */
+    }
+    printf("\n");
+}
